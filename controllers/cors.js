99f9be4b4bfe44b57f7887bb2b9e7378a95bc4e1
Karthik Balakrishnan <torcellite@gmail.com>
2015-10-09 19:03:26 +0530

    CORS update

--- controllers/cors.js
+++ controllers/cors.js
@@ -3,14 +3,20 @@
 var config = require('../config/config');
 
 module.exports = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', config.frontend);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Length, Content-Type, X-Requested-With');
-    res.header('Access-Control-Allow-Resource', '*');
-    // intercept OPTIONS method
-    if ('OPTIONS' === req.method) {
-        res.sendStatus(200);
-    } else {
-        next();
+    var idx = config.frontend.indexOf(req.headers.origin);
+    var isPreFlight = false;
+    if (idx > -1) {
+        res.header('Access-Control-Allow-Origin', config.frontend[idx]);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Length, Content-Type, X-Requested-With');
+        res.header('Access-Control-Allow-Resource', '*');
+        // intercept OPTIONS method
+        if ('OPTIONS' === req.method) {
+            isPreFlight = true;
+            res.sendStatus(200);
+        }
     }
+    if (!isPreFlight)
+        next();
+
 };
